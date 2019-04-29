75ddd8a30d2b99140b91260d2034ed20a38ec83b
Joe Drago <jdrago@netflix.com>
2019-04-29 16:51:50 -0700

    Add "npm run test"

---
+++ test.bat
@@ -0,0 +1,19 @@
+@echo off
+
+echo "this is [0;30mDark Black text[0m right here"
+echo "this is [0;31mDark Red text[0m right here"
+echo "this is [0;32mDark Green text[0m right here"
+echo "this is [0;33mDark Yellow text[0m right here"
+echo "this is [0;34mDark Blue text[0m right here"
+echo "this is [0;35mDark Purple text[0m right here"
+echo "this is [0;36mDark Cyan text[0m right here"
+echo "this is [0;37mDark White text[0m right here"
+
+echo "this is [1;30mBright Black text[0m right here"
+echo "this is [1;31mBright Red text[0m right here"
+echo "this is [1;32mBright Green text[0m right here"
+echo "this is [1;33mBright Yellow text[0m right here"
+echo "this is [1;34mBright Blue text[0m right here"
+echo "this is [1;35mBright Purple text[0m right here"
+echo "this is [1;36mBright Cyan text[0m right here"
+echo "this is [1;37mBright White text[0m right here"
